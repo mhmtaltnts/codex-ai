8decc148351ca336f47c00f9e4a1694031fee839
Muhammet Altuntaş <47993275+mhmtaltnts@users.noreply.github.com>
2022-12-27 19:04:56 +0300

    Update script.js

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codexai-r1kb.onrender.com', {
+    const response = await fetch('https://codexai-1.onrender.com', {
       method: 'POST',
       headers: {
         "Content-Type": "application/json",
